ec94b3c9142486a6467e94511034e40295eb4dbd
gfranco <gfranco@CompSciICv-20.fsc.int>
2025-03-19 13:38:22 -0400

    Updated SQL tables, created footer, added manager and employee registration pages. No styles or special formatting added yet.

---
+++ manager.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Managers</title>
+    </head>
+    <body>
+        <h2>Manager Page</h2>
+
+        <a href="signup.php">Click here to add new customers</a>
+        <a href="register.php">Click here to add new employees</a>
+
+        <br>
+
+        <?php include 'footer.php' ?>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Register</title>
+    </head>
+    <body>
+<h2>Employee Registration Form</h2>
+
+      
+
+        <br>
+
+        <?php include 'footer.php' ?>
+    </body>
+</html>

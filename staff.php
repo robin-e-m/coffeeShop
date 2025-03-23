bd670e45d23eddc35ce3865f8e26699bbd518baa
egarrit2 <egarrit2@student.fitchburgstate.edu>
2025-03-23 12:21:37 -0400

    Add files via upload
    index has now footer and header, with some simple cosmetic features. staff just has header and footer. Footer and DBConnect no change to code.

---
+++ staff.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>text</title>
+  <br>
+  <?php include 'header.php' ?>
+</head>
+<body>
+    <div class="boxed">
+        <style> 
+        body  {
+        background-color: lightblue
+        }
+        </style>
+        
+    </div>
+    <h1 style="text-align:center;"> work in progress :) </h1>
+  <br>
+  <?php include 'footer.php' ?>
+</body>
+</html>

4793bee361851779b7f5e2d7870ea6bc94b5ae15
gfranco <gfranco@CompSciICv-20.fsc.int>
2025-03-26 10:38:48 -0400

    Fixed session control

--- customer.php
+++ customer.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
-
+<?php
+include 'header.php';
+if (!(isset($_SESSION['usertype'])) or $usertype != 3) {
+    header("Location:index.php");
+    exit;
+}
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Customer</title>
-        <?php include "header.php" ?>
+
     </head>
     <body>
         <h2>Customer Page</h2>
         <p> Customer profile will go on this page</p>
-        
-        
+
+
         <br>
         <br>
         <?php include "footer.php" ?>
--- registerStaff.php
+++ registerStaff.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-
+<?php
+include 'header.php';
+if (!(isset($_SESSION['usertype'])) or $usertype != 1) {
+    header("Location:index.php");
+    exit;
+}
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Register Staff</title>
-         <?php include "header.php" ?>
-        
+
     </head>
     <body>
         <h1>Staff Registration</h1>

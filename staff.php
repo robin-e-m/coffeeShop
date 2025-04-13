a9ec2c722fd780842ed41cf3e6e2090488393f16
egarrit2 <egarrit2@CompSciICv-18.fsc.int>
2025-04-13 13:52:38 -0400

    has a new form for menu item

--- staff.php
+++ staff.php
@@ -14,11 +14,42 @@ if (!(isset($_SESSION['usertype'])) or $usertype != 2) {
 </head>
 
 <body>
-    <h1> Staff Page</h1>
-
-    <p>WORK IN PROGRESS</p>
-    <br>
-    <br>
+    <style> 
+   body  {
+  background-color: lightblue
+        }
+        </style>
+  <div class="w3-container">
+    <h1>New Item for menu </h1>
+    <form name="register" action="menuregisterAction.php" method="get">
+      <div class="item">
+        <label>Name of item</label>
+        <input type="text" name="name" size="40" required/>
+      </div>
+        <label>desrciption of item </label>
+        <input type="text" name="description" size="40" />
+      </div>
+      <div class="item">
+        <label>price of item </label>
+        <input type="text" name="price" size="40" />
+      </div>
+      <div class="item">
+        <label>What Category does your new item fall into?</label>
+        <select name="category" required>
+          <option disabled selected value>Select category</option>
+          <option value="hot drink">hot drink</option>
+          <option value="cold drink">cold drink</option>
+          <option value="bakery">bakery</option>
+          <option value="seasonal">seasonal</option>
+        </select>
+      </div>
+      <div class="item">
+        <input type="submit" value="Submit" />
+        <input type="reset" value="Reset" />
+      </div>
+    </form>
+  </div>
+  <br>
     <?php include 'footer.php' ?>
 </body>
 </html>
